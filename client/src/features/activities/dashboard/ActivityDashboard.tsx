2c6baa52a9ce333b1fe42b3d08f3d40caf65e907
Rafael Silva <rafael.silva.06@outlook.com>
2026-09-23 16:35:10 -0600

    53.Deleting an activity

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -12,6 +12,7 @@ type Props = {
   closeForm: () => void;
   editMode: boolean;
   submitForm: (activity: Activity) => void;
+  deleteActivity: (id: string) => void;
 }
 
 export default function ActivityDashboard({ activities,
@@ -21,7 +22,8 @@ export default function ActivityDashboard({ activities,
   openForm,
   closeForm,
   editMode,
-  submitForm
+  submitForm,
+  deleteActivity
 }: Props) {
   return (
     <Grid2 container spacing={3}>
@@ -29,6 +31,7 @@ export default function ActivityDashboard({ activities,
         <ActivityList
           activities={activities}
           selectActivity={selectActivity}
+          deleteActivity={deleteActivity}
         />
       </Grid2>
       <Grid2 size={5}>
