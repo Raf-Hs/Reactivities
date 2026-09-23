d4c2a710947932b2db9df04230af4baa8cc87529
Rafael Silva <rafael.silva.06@outlook.com>
2026-09-23 11:03:57 -0600

    48. Selecting an individual activity to view.

--- client/src/features/activities/dashboard/ActivityCard.tsx
+++ client/src/features/activities/dashboard/ActivityCard.tsx
@@ -2,21 +2,22 @@ import { Button, Card, CardActions, CardContent, Chip, Typography } from "@mui/m
 
 type Props = {
     activity: Activity
+    selectActivity: (id: string) => void;
 }
 
-export default function ActivityCard({ activity }: Props) {
+export default function ActivityCard({ activity, selectActivity }: Props) {
     return (
         <Card sx={{ borderRadius: 3 }}>
             <CardContent>
                 <Typography variant="h5"> {activity.title}</Typography>
                 <Typography sx={{ color: 'text.secondary', mb: 1 }}>{activity.title}</Typography>
                 <Typography variant="body2">{activity.description}</Typography>
-                <Typography variant="subtitle1">{activity.city}/activity.venue</Typography>
+                <Typography variant="subtitle1">{activity.city} / {activity.venue}</Typography>
             </CardContent>
 
             <CardActions sx={{ display: 'flex', justifyContent: 'space-between', pb: 2 }}>
                 <Chip label={activity.category} variant="outlined" />
-                <Button size="medium" variant="contained">View</Button>
+                <Button onClick={() => selectActivity(activity.id)} size="medium" variant="contained">View</Button>
             </CardActions>
 
         </Card>
